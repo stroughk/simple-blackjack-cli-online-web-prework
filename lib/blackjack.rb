d49309735270c8648c39b4629938e927f490a909
Kateryna Strough <katerynastrough@hotmail.com>
2019-04-21 15:38:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,68 +1,78 @@
+##################
+# shared methods #
+##################
+
 def welcome
- puts "Welcome to the Blackjack Table" # code #welcome here
+  # code #welcome here
 end
 
 def deal_card
-  rand(1..11) # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  puts "Your cards add up to #{card_total}" # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
+
+
+##########
+# rounds #
+##########
+
+def welcome
+  puts "Welcome to the Blackjack Table"
+end
+
 
 def prompt_user
   puts "Type 'h' to hit or 's' to stay"
-  # code #prompt_user here
+end
+
+
+def initial_round
+  first_round = deal_card + deal_card
+  display_card_total(first_round)
+  return first_round
 end
 
 def get_user_input
-  gets.chomp.strip # code #get_user_input here
+  gets.chomp.strip
 end
 
-def end_game
- puts "Sorry, you hit#{card_total}. Thanks for playing!" # code #end_game here
-end
-
-def initial_round
- first_round=deal_card+deal_card
- display_card_total(first_round)
- return first_round
- 
- 2  # code #initial_round here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def hit?(card_total)
- prompt_user
- input = get_user_input
- until input == 'h' || input == 's'
- invalid_command
- prompt_user
- input = get_user_input
- end
- if input == 'h'
-   card_total += deal_card
-   elsif input == 's'
-   card_total
- end
-  # code hit? here
+  prompt_user
+  input = get_user_input
+  until input == 'h' || input == 's'
+    invalid_command
+    prompt_user
+    input = get_user_input
+  end
+  if input == 'h'
+    card_total += deal_card
+  elsif input == 's'
+    card_total
+  end
 end
+
 
 def invalid_command
   puts "Please enter a valid command"
-  # code invalid_command here
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+##########
+# runner #
+##########
 
 def runner
   welcome
   card_total = initial_round
-  until card_total = hit?(card_total)
-  display_card_total(card_total)
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
-end_game(card_total) 
-  # code runner here
-end
-    
